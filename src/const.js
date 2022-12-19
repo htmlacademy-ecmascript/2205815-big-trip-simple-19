b2ceeb62b6dfc7431b06f8ca19154614326ade34
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-12-19 19:07:15 +0300

    const

---
+++ src/const.js
@@ -0,0 +1,2 @@
+const offersByType = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+const pointsByType = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
